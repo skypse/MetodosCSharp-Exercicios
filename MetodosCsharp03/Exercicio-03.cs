508677aaba8b302b1b1ee89073409a55f68d7ccd
Gabriel Do Amaral Alves <skypse0136@gmail.com>
2024-06-25 19:00:30 -0300

    exercicio 03

---
+++ MetodosCsharp03/Exercicio-03.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Exercício 3: Método com Parâmetros e Retorno:
+//Descrição: Crie um método chamado Somar que receba dois inteiros como parâmetros e retorne a soma desses dois números. 
+
+namespace MetodosCsharp03
+{
+    class Exercicio_03
+    {
+        public static int Soma(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static void Main(string[] args)
+        {
+            int resultado = Soma(5, 5);
+            Console.WriteLine($"O resultado da soma é {resultado}");
+        }
+    }
+}
