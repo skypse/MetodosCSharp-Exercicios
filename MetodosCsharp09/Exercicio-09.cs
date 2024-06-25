2ee63f67e0fc95b2c9ccff1c768619405bc42b45
Gabriel Do Amaral Alves <skypse0136@gmail.com>
2024-06-25 19:26:08 -0300

    exercicio 09

---
+++ MetodosCsharp09/Exercicio-09.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Exercício 9: Método com Parâmetros e Retorno:
+//Descrição: Crie um método chamado ConcatenarStrings que receba duas strings como parâmetros e retorne a concatenação dessas duas strings. 
+
+namespace MetodosCsharp09
+{
+    class Exercicio_09
+    {
+        public static string ConcatenarStrings(string string1, string string2)
+        {
+            return string1 + string2;
+        }
+
+        public static void Main()
+        {
+            string resultado = ConcatenarStrings("Testando, ", "Concatenação!");
+            Console.WriteLine($"Resultado da concatenação: {resultado}");
+        }
+    }
+}
