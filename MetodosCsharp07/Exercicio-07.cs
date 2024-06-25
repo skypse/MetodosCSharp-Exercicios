bbb22ebc7544206dd11bd119ea5fe7c4c933e066
Gabriel Do Amaral Alves <skypse0136@gmail.com>
2024-06-25 19:18:07 -0300

    exercicio 07

---
+++ MetodosCsharp07/Exercicio-07.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Exercício 7: Método sem Parâmetros com Retorno:
+//Descrição: Crie um método chamado ObterDataAtual que não receba parâmetros e retorne a data atual como uma string no formato "dd/MM/yyyy". 
+
+namespace MetodosCsharp07
+{
+    class Exercicio_07
+    {
+        public static string ObterDataAtual()
+        {
+            return DateTime.Now.ToString("dd/MM/yyyy");
+        }
+
+        public static void Main(string[] args)
+        {
+            string dataAtual = ObterDataAtual();
+            Console.WriteLine($"A data de hoje é: {dataAtual}");
+        }
+    }
+}
