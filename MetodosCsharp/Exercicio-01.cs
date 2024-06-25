adaace815cbc8558ada7b91dd4db4127187da93f
Gabriel Do Amaral Alves <skypse0136@gmail.com>
2024-06-25 18:57:43 -0300

    exercicio 02

--- MetodosCsharp/Exercicio-01.cs
+++ MetodosCsharp/Exercicio-01.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-// Exercício 1: Método sem Parâmetros e sem Retorno 
+// Exercício 1: Método sem Parâmetros e sem Retorno: 
 // Descrição: Crie um método chamado "ImprimirSaudacao" que não receba parâmetros e apenas imprima "Olá, bem-vindo ao curso de C#!" na tela.
 
 namespace MetodosCsharp01
---
+++ MetodosCsharp02/Exercicio-02.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Exercício 2: Método com Parâmetro sem Retorno:
+//Descrição: Crie um método chamado "ImprimirMensagem" que receba uma string como parâmetro e imprima essa mensagem na tela. 
+
+namespace MetodosCsharp02
+{
+    class Exercicio_02
+    {
+        public static void ImprimirMensagem(string mensagem)
+        {
+            Console.WriteLine(mensagem);
+        }
+
+        public static void Main(string[] args)
+        {
+            ImprimirMensagem("Testando passagem de parâmetro mensagem");
+        }
+    }
+}
