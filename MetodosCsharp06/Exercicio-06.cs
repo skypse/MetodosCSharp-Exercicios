4c6da1d8c65a6a16378e35b3fc0dee764e5991d9
Gabriel Do Amaral Alves <skypse0136@gmail.com>
2024-06-25 19:14:09 -0300

    exercicio 06

---
+++ MetodosCsharp06/Exercicio-06.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Exercício 6: Método com Parâmetros e Retorno:
+//Descrição: Crie um método chamado EhPar que receba um inteiro como parâmetro e retorne verdadeiro se o número for par e falso se for ímpar. 
+
+namespace MetodosCsharp06
+{
+    class Exercicio_06
+    {
+        public static bool EhPar(int numero)
+        {
+            return numero % 2 == 0;
+        }
+
+        public static void Main(string[] args)
+        {
+            bool resultado = EhPar(4); // verdade
+            Console.WriteLine($"O número é par?: {resultado}");
+        }
+    }
+}
