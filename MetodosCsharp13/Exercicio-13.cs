601ee815297c3db75090cafb6e4475ac75d594be
Gabriel Do Amaral Alves <skypse0136@gmail.com>
2024-06-27 19:55:53 -0300

    add exercicio 13

---
+++ MetodosCsharp13/Exercicio-13.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Exercício 13: Método com Parâmetros e Retorno:
+//Descrição: Crie um método chamado CalcularAreaRetangulo que receba a largura e a altura de um retângulo como parâmetros e retorne a área desse retângulo.
+
+namespace MetodosCsharp13
+{
+    class Exercicio_13
+    {
+        public static double CalcularAreaRetangulo(double largura, double altura)
+        {
+            return largura * altura;
+        }
+
+        public static void Main(string[] args)
+        {
+            double area = CalcularAreaRetangulo(10.0, 15.0);
+            Console.WriteLine($"Área do retângulo: {area}");
+        }
+    }
+}
