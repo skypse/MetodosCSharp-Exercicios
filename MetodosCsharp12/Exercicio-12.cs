7271d5b4085bf6a8b6a2e8631f6886f4d32008bf
Gabriel Do Amaral Alves <skypse0136@gmail.com>
2024-06-27 19:49:57 -0300

    add exercicio 12

---
+++ MetodosCsharp12/Exercicio-12.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Exercício 12: Método com Parâmetro sem Retorno:
+//Descrição: Crie um método chamado ImprimirQuadrado que receba um inteiro como parâmetro e imprima o quadrado desse número. 
+
+namespace MetodosCsharp12
+{
+    class Exercicio_12
+    {
+        public static void ImprimirQuadrado(int numero)
+        {
+            Console.WriteLine(numero * numero);
+        }
+
+        public static void Main(string[] args)
+        {
+            ImprimirQuadrado(4);
+        }
+    }
+}
