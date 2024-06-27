798e6f2943a695172aea049bc6b17ea58602db2f
Gabriel Do Amaral Alves <skypse0136@gmail.com>
2024-06-27 19:35:34 -0300

    add exercicio 11

---
+++ MetodosCsharp11/Exercicio-11.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Exercício 11: Método sem Parâmetros e sem Retorno:
+//Descrição: Crie um método chamado ExibirHoraAtual que não receba parâmetros e apenas imprima a hora atual no formato "HH:mm" na tela.
+
+namespace MetodosCsharp11
+{
+    class Exercicio_11
+    {
+        public static void ExibirHoraAtual()
+        {
+            Console.WriteLine(DateTime.Now.ToString("HH:mm"));
+        }
+
+        public static void Main(string[] args)
+        {
+            ExibirHoraAtual();
+        }
+    }
+}
