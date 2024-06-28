a69a9338d14576f73a46a952b1460711be42ed4d
Gabriel Do Amaral Alves <skypse0136@gmail.com>
2024-06-28 15:35:16 -0300

    add exercicio 16

---
+++ MetodosCsharp16/Exercicio-16.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Exercício 16: Método com Parâmetros e Retorno:
+//Descrição: Crie um método chamado EhPrimo que receba um inteiro como parâmetro e retorne verdadeiro se o número for primo e falso caso contrário. 
+
+namespace MetodosCsharp16
+{
+    class Exercicio_16
+    {
+        public static bool EhPrimo(int numero)
+        {
+            if (numero <= 1) 
+                return false;
+            for (int i = 2; i <= Math.Sqrt(numero); i++)
+            {
+                if (numero % i == 0) 
+                    return false;
+            }
+            return true;
+        }
+
+        public static void Main(string[] args)
+        {
+            int[] numeros = { 2, 3, 4, 5, 16, 17};
+
+            foreach (int numero in numeros)
+            {
+                bool resultado = EhPrimo(numero);
+                Console.WriteLine($"O número {numero} é primo? {resultado}");
+            }
+        }
+    }
+}
