1e49be472267d627983797626a54762e76e8bcf5
Gabriel Do Amaral Alves <skypse0136@gmail.com>
2024-06-28 15:19:20 -0300

    add exercicio 14

---
+++ MetodosCsharp14/Exercicio-14.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Exercício 14: Método com Parâmetros e Retorno:
+//Descrição: Crie um método chamado CalcularPerimetroCirculo que receba o raio de um círculo como parâmetro e retorne o perímetro do círculo
+
+namespace MetodosCsharp14
+{
+    class Exercicio_14
+    {
+        public static double CalcularPerimetroCirculo(double raio)
+        {
+            return 2 * Math.PI * raio;
+        }
+
+        public static void Main(string[] args)
+        {
+            double perimetro = CalcularPerimetroCirculo(4.0);
+            Console.WriteLine($"Perímetro do círculo: {perimetro}");
+        }
+    }
+}
