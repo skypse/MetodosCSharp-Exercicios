267a3c48ac6a8f3bee96b471008487a5a35c8e2f
Gabriel Do Amaral Alves <skypse0136@gmail.com>
2024-06-29 10:12:46 -0300

    add exercicio 18 e 19

---
+++ MetodosCsharp18/Exercicio-18.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Exercício 18: Método com Parâmetros e Retorno:
+//Descrição: Crie um método chamado ConverterCelsiusParaFahrenheit que receba uma temperatura em graus Celsius como parâmetro e retorne a temperatura em Fahrenheit. 
+
+namespace MetodosCsharp18
+{
+    class Exercicio_18
+    {
+        public static double ConverterCelsiusParaFahrenheit(double celsius)
+        {
+            return (celsius * 9 / 5) + 32;
+        }
+
+        public static void Main(string[] args)
+        {
+            double celsius = 25.0;
+            double fahrenheit = ConverterCelsiusParaFahrenheit(celsius);
+            Console.WriteLine($"Temperatura em Fahrenheit: {fahrenheit}");
+        }
+    }
+}
---
+++ MetodosCsharp19/Exercicio-19.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Exercício 19: Método com Parâmetros e Retorno 
+//Descrição: Crie um método chamado InverterString que receba uma string como parâmetro e retorne a string invertida. 
+
+namespace MetodosCsharp19
+{
+    class Exercicio_19
+    {
+        public static string InverterString(string texto)
+        {
+            char[] array = texto.ToCharArray();
+            Array.Reverse(array);
+            return new string(array);
+        }
+
+        public static void Main(string[] args)
+        {
+            string texto = " Amaral";
+            string invertida = InverterString(texto);
+            Console.WriteLine($"String invertida: {invertida}");
+        }
+    }
+}
